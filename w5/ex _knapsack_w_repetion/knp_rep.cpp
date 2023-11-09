85c1d53acb339d732e18df8ac69ff2785f7cf20f
Daniel Núñez <daniel.portonunez@gmail.com>
2023-11-09 18:21:49 -0300

    feat: add kn_rep_dynamic

---
+++ w5/ex _knapsack_w_repetion/knp_rep.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using std::vector;
+using std::min;
+using std::cout;
+
+
+int knap(int n, int capacity, vector<int> &v, vector<int> &w) {
+    vector<int> chunk_values(capacity+1);
+    chunk_values[0] = 0;
+
+    for(int i=1; i<=capacity; i++) {
+        int value = 0;
+        for (int j = 0; j < n; j++) {
+            if (w[j] <= i) {
+                int value_temp = chunk_values[i - w[j]] + v[j];
+                if (value_temp > value) {
+                    value = value_temp;
+                }
+            }
+        }
+        chunk_values[i] = value;
+    }
+
+    return chunk_values[capacity];
+}
+
+int main() {
+    int n;
+    int capacity;
+    std::cin >> n >> capacity;
+    vector<int> values(n);
+    vector<int> weights(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> values[i] >> weights[i];
+    }
+
+    std::cout << knap(n, capacity, values, weights);
+    return 0;
+}
